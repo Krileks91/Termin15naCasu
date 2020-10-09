69d3967c65c2b88279094abcf6aa0a9014275029
Krileks91 <pandorian91@gmail.com>
2020-10-09 20:23:01 +0200

    AdapterView

---
+++ app/src/main/java/com/example/termin15nacasu/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.termin15nacasu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.accessibilityservice.GestureDescription;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+    List<String> data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        list = findViewById(R.id.list);
+        data = getNames();
+
+       /* final ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                data);*/
+        //Pozivamo da nam prikaze Elemente od 1-16
+        final SongAdapter adapter = new SongAdapter(getSongs(), this);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Kliknuli ste na pesmu " + adapter.getItem(i).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private List<String> getNames(){
+        List<String> names = new ArrayList<>();
+        for(int i = 1; i < 16; i++){
+            names.add("Element "+ i);
+        }
+        return names;
+    }
+    private List<Song> getSongs(){
+        List<Song> songs = new ArrayList<>();
+        for(int i = 1; i < 16; i++){
+            songs.add(new Song("Author " + i, "Names" + i));
+        }
+        return songs;
+    }
+
+}
---
+++ app/src/main/java/com/example/termin15nacasu/Song.java
@@ -0,0 +1,27 @@
+package com.example.termin15nacasu;
+
+public class Song {
+    private String author;
+    private String name;
+
+    public Song(String author, String name) {
+        this.author = author;
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
